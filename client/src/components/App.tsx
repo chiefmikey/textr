d6d06c68fc8ec616d94bfb961d34153af833a63d
chiefmikey <wolfemikl@gmail.com>
2022-11-25 21:40:19 -0700

    Init root

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,12 +1,12 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import _ from "lodash";
+import _ from 'lodash';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 const App = () => {
   const dispatch = useDispatch();
   const [isLoading, setIsLoading] = useState(false);
   const mainState = useSelector((state) => state);
-  return (<></>)
+  return <></>;
 };
 
 export default App;
--- client/src/environment.ts
+++
@@ -1,9 +0,0 @@
-const environment = {
-  serverInstanceId: 'i-0d42af2d2c4d177ac',
-  dnsInstanceId: 'i-09640b64556a6fb77',
-  region: 'us-east-2',
-  identityPoolId: 'us-east-2:9de629c9-c774-42ef-b641-d06eb430f8d8',
-  secretName: 'repo',
-};
-
-export default environment;
