01eed9d6d532a9cb45078a60be16eb91136177ea
chiefmikey <wolfemikl@gmail.com>
2022-11-25 22:13:31 -0700

    Update koa format

--- client/app.ts
+++ client/app.ts
@@ -1,14 +1,35 @@
-import path from 'node:path';
+import { createReadStream } from 'node:fs';
 
 import Koa from 'koa';
 import serve from 'koa-static';
 
-const port = 3000;
-
 const app = new Koa();
+const port = 3000;
+const redirect = 'docs';
+const subdir = 'public';
 
 app
-  .use(serve(path.join(path.resolve(), 'docs')))
+  .use(serve(redirect))
+  .use(async (context, next) => {
+    try {
+      if (context.status === 404) {
+        const url = context.url.split(subdir)[1];
+        if (url) {
+          const path = `${redirect}/${subdir}${url}`;
+          context.type = 'html';
+          context.body = createReadStream(path);
+        } else {
+          context.type = 'html';
+          context.body = createReadStream(`${redirect}/404.html`);
+        }
+      } else {
+        await next();
+      }
+    } catch {
+      context.type = 'html';
+      context.body = createReadStream(`${redirect}/404.html`);
+    }
+  })
   .listen(port, () =>
     console.log(`Koa is listening at http://localhost:${port}`),
   );
